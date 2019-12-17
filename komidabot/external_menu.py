318de3d08b6b0dd8b2bb1ae5ab1f1724c37e0c02
heldplayer <mitchel.pyl+github@gmail.com>
2019-12-18 00:36:03 +0100

    Disabled course type tagging via sort order because campuses without hot foods also use it

--- komidabot/external_menu.py
+++ komidabot/external_menu.py
@@ -244,16 +244,17 @@ class ExternalMenu:
                             course_type = models.FoodType.VEGAN if vegan else models.FoodType.MEAT
 
                     # If a sort order is set, use it instead
-                    if sort_order == 1:
-                        course_type = models.FoodType.VEGAN
-                    elif sort_order == 2:
-                        course_type = models.FoodType.MEAT
-                    elif sort_order == 3:
-                        course_type = models.FoodType.PASTA_VEGAN
-                    elif sort_order == 4:
-                        course_type = models.FoodType.PASTA_MEAT
-                    elif sort_order == 5:
-                        course_type = models.FoodType.GRILL
+                    # FIXME: Disabled because campuses without hot foods also use the sort order
+                    # if sort_order == 1:
+                    #     course_type = models.FoodType.VEGAN
+                    # elif sort_order == 2:
+                    #     course_type = models.FoodType.MEAT
+                    # elif sort_order == 3:
+                    #     course_type = models.FoodType.PASTA_VEGAN
+                    # elif sort_order == 4:
+                    #     course_type = models.FoodType.PASTA_MEAT
+                    # elif sort_order == 5:
+                    #     course_type = models.FoodType.GRILL
 
                     menu_item = ExternalMenuItem(sort_order, course_type, menu_contents)
 
