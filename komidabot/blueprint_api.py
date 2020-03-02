55f0c926b94aaebf26b70f659e024413abb6fef9
heldplayer <mitchel.pyl+github@gmail.com>
2020-03-02 02:27:18 +0100

    Add additional API endpoint

--- komidabot/blueprint_api.py
+++ komidabot/blueprint_api.py
@@ -30,9 +30,47 @@ def get_campus_list():
             'id': campus.id,
             'name': campus.name,
             'short_name': campus.short_name,
+            # TODO: Needs opening hours
         })
 
     return jsonify({'campuses': result})
+
+
+@blueprint.route('/campus/closing_days/<day_str>', methods=['GET'], defaults={'short_name': None})
+@blueprint.route('/campus/<short_name>/closing_days/<day_str>', methods=['GET'])
+def get_active_closing_days(short_name: str, day_str: str):
+    """
+    Gets all currently active closures.
+    """
+
+    if short_name is None:
+        campuses = models.Campus.get_all_active()
+    else:
+        campus = models.Campus.get_by_short_name(short_name)
+
+        if campus is None:
+            return abort(400)
+
+        campuses = [models.Campus.get_by_short_name(short_name)]
+
+    result = {}
+
+    try:
+        day_date = date.fromisoformat(day_str)
+    except ValueError:
+        return abort(400)
+
+    for campus in campuses:
+        closed_data = models.ClosingDays.find_is_closed(campus, day_date)
+
+        if closed_data is not None:
+            result[campus.short_name] = {
+                'first_day': closed_data.first_day.isoformat(),
+                'last_day': closed_data.last_day.isoformat(),
+                'reason': str(translatable_to_object(closed_data.translatable)),
+            }
+
+    return jsonify({'closing_days': result})
 
 
 @blueprint.route('/campus/closing_days/<from_str>/<to_str>', methods=['GET'], defaults={'short_name': None})
